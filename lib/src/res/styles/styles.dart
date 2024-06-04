ad10fc1f2565b0a25771e677a4a8e3dd3c011b50
Enoch <enoch_aik@icloud.com>
2024-06-05 01:20:32 +0200

    - added extended configuration to app - added assets dir

---
+++ lib/src/res/styles/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppStyles {
+  const AppStyles._();
+
+  static const TextStyle textStyle = TextStyle(fontFamily: 'EudoxusSans');
+  static InputDecoration inputDecoration =
+      InputDecoration(contentPadding: EdgeInsets.symmetric(horizontal: 8.w));
+}
---
+++ lib/src/router/provider.dart
@@ -0,0 +1,4 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rccg_jp/src/router/router.dart';
+
+final appRouter = Provider((ref) => AppRouter());
