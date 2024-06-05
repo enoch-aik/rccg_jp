bc066e38500dccad691289a9289550ba3cc2254c
Enoch <enoch_aik@icloud.com>
2024-06-05 02:46:38 +0200

    - added onboarding UI

---
+++ lib/lib.dart
@@ -0,0 +1,6 @@
+export 'package:flutter/material.dart';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'package:auto_route/auto_route.dart';
+export 'package:rccg_jp/src/router/router.dart';
+export 'package:rccg_jp/src/router/navigator.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
---
+++ lib/src/extensions/dates.dart
@@ -0,0 +1,78 @@
+import 'package:intl/intl.dart';
+
+extension DateConverter on DateTime {
+  // Returns the time in a readable format
+  String get toTime => DateFormat.Hm().format(this);
+
+  // Returns the date in a readable format
+  String toGroupName([bool weekday = true]) {
+    final now = DateTime.now();
+
+    if (toDateString() == now.toDateString()) {
+      return 'Today';
+    }
+
+    if (toDateString() ==
+        now.subtract(const Duration(days: 1)).toDateString()) {
+      return 'Yesterday';
+    }
+
+    String format = '';
+    if (weekday) {
+      format += '${DateFormat.WEEKDAY}, ';
+    }
+
+    format += '${DateFormat.DAY} '
+        '${DateFormat.ABBR_MONTH}, ${DateFormat.YEAR}';
+
+    return DateFormat(format).format(this);
+  }
+
+  // Returns the date in a readable format
+  String toDateString() {
+    final format = DateFormat.yMd();
+    return format.format(this).toLowerCase();
+  }
+
+  // Returns the date and time in a readable format
+  String toDateAndTime() {
+    final now = DateTime.now();
+    final difference = differenceInDays(now, this);
+
+    if (difference >= 7) {
+      final formatter = DateFormat('MMMM d \'at\' h:mm a');
+      return formatter.format(this);
+    } else {
+      final formatter = DateFormat('EEEE \'at\' h:mm a');
+      return formatter.format(this);
+    }
+  }
+
+  // Returns the difference in days between two dates
+  int differenceInDays(DateTime a, DateTime b) {
+    final difference = a.difference(b).inDays;
+    return difference.abs();
+  }
+
+  // Returns the difference in hours and day between two dates
+  String timeAgo() {
+    final now = DateTime.now();
+    final difference = now.difference(this);
+
+    if (difference.inSeconds < 60) {
+      return "${difference.inSeconds} secs ago";
+    } else if (difference.inMinutes < 60) {
+      return "${difference.inMinutes} mins ago";
+    } else if (difference.inHours < 48) {
+      return "${difference.inHours} hrs ago";
+    } else {
+      final formatter = DateFormat.yMMMMd();
+      return formatter.format(this);
+    }
+  }
+
+  // check if two dates are the same day
+  bool isSameDay(DateTime other) {
+    return year == other.year && month == other.month && day == other.day;
+  }
+}
